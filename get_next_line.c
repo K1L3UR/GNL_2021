61b31011c0411797b6d9ef15a99770330aa13c40
arnaud durand <Wesper@macbook-pro-de-arnaud.home>
2021-02-20 02:38:11 +0100

    gnl test working

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 
 */
 
-static ssize_t	gnl_read(int fd, char *stock)
+static ssize_t	gnl_read(int fd, char **stock)
 {
 	ssize_t	ret;
 	char	buf[BUFFER_SIZE];
@@ -14,43 +14,47 @@ static ssize_t	gnl_read(int fd, char *stock)
 	size_t	stock_len;
 
 	stock_len = 0;
-	ret = 0;
-	while (stock == NULL || strchr(stock, '\n') == NULL)
+	ret = 1;
+	while (*stock == NULL || strchr(*stock, '\n') == NULL)
 	{
 		if ((ret = read(fd, buf, BUFFER_SIZE)) <= 0)
 			return (ret);
-		if (stock != NULL)
-			stock_len = strlen(stock);
+		if (*stock != NULL)
+			stock_len = strlen(*stock);
 		if (!(string = calloc(ret + stock_len + 1, 1)))
 			return (-1);
-		memcpy(string, stock, stock_len); // verif implementation ft_memcpy -> src = NULL stock_len = 0 | 
+		memcpy(string, *stock, stock_len); // verif implementation ft_memcpy -> src = NULL stock_len = 0 | 
 		memcpy(string + stock_len, buf, ret);
-		if (stock)
-			free(stock);
-		stock = string;
+		if (*stock)
+			free(*stock);
+		*stock = string;
 	}
 	return (ret);
 }
 
-static	int gnl_fill_line(char *stock, char **line)
+static	int gnl_fill_line(char **stock, char **line)
 {
 	char	*position_eol;
 	char	*tmp;
 
-	position_eol = strchr(stock, '\n');
+	if (stock == NULL)
+		return (0);
+	position_eol = strchr(*stock, '\n');
 	if (position_eol != NULL)
 	{
-		if ((*line = calloc(position_eol - stock + 1, 1)) == NULL)
+		if ((*line = calloc(position_eol - *stock + 1, 1)) == NULL)
 			return (-1);
-		memcpy(*line, stock, position_eol - stock);
+		memcpy(*line, *stock, position_eol - *stock);
 		if ((tmp = calloc(strlen(position_eol) + 1, 1)) == NULL)
 			return (-1);
 		memcpy(tmp, position_eol + 1, strlen(position_eol));
+		free(*stock);
+		*stock = tmp;
 	}
 	else
 	{
-		*line = stock;
-		stock = NULL;
+		*line = *stock;
+		*stock = NULL;
 	}
 	return (0);
 }
@@ -58,12 +62,15 @@ static	int gnl_fill_line(char *stock, char **line)
 int		get_next_line(int fd, char **line)
 {
 	static	char	*stock = NULL;
+	int		ret;
 
 	if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
 		return (-1);
-	if (gnl_read(fd, stock) == -1)
+	if ((ret = gnl_read(fd, &stock)) == -1)
 		return (-1);
-	if (gnl_fill_line(stock, line) == -1)
+	if (gnl_fill_line(&stock, line) == -1)
 		return (-1);
-	return (0);
+	if (ret == 0)
+		return (ret);
+	return (1);
 }
--- main.c
+++ main.c
@@ -3,21 +3,31 @@
 #include <sys/types.h>
 #include "get_next_line.h"
 
-int	main(int argc, char **argv)
+int    main(int argc, char **argv)
 {
-	int	fd;
-	char	*line;
-	int	ret;
+    int        fd;
+    int        fd_w;
+    char    *line;
+    int        ret;
 
-	ret = 1;
-	if (argc < 2)
-		return (0);
-	fd = open(argv[1], O_RDONLY);
-	while (ret > 0)
-	{
-		ret = get_next_line(fd, &line);
-		printf("%i-%s|\n", ret, line);
-		sleep(1);
-	}
-	return (0);
+    if (argc < 2)
+        return (0);
+    fd = open(argv[1], O_RDONLY);
+    fd_w = open("gnl_out.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR + S_IWUSR);
+    if (fd < 0)
+    {
+        printf("fd = %i\n", fd);
+        return (1);
+    }
+    ret = 1;
+    line = NULL;
+    while (ret > 0)
+    {
+        ret = get_next_line(fd, &line);
+        printf("%i-%s\n", ret, line);
+        write(fd_w, line, strlen(line));
+        if (ret != 0)
+            write(fd_w, "\n", 1);
+    }
+    return (0);
 }
